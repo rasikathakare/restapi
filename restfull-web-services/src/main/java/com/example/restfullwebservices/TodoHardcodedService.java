3bee2e48d7030e727b9b475fc8be48b22936c362
sarthak-97 <sarthakgupta115@gmail.com>
2021-01-17 22:20:49 +0530

    jpa methods added

--- restfull-web-services/src/main/java/com/example/restfullwebservices/TodoHardcodedService.java
+++ restfull-web-services/src/main/java/com/example/restfullwebservices/TodoHardcodedService.java
@@ -6,54 +6,46 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 
 
 @Service
-public class TodoHardcodedService {
+public class TodoHardcodedService  {
 	
-	private static List<Todo> todos = new ArrayList<Todo>();
-	private static int idCounter = 0;
+	
 
-	public List<Todo> findAll() {
-		return todos;
+	@Autowired
+	private TodoRepository todoRepository;
+	
+	
+
+
+	public Iterable<Todo> findAll() {
+		return todoRepository.findAll();
 	}
 
 
 	
 	public Todo save(Todo todo) {
-		if(todo.getId()==-1 || todo.getId()==0) {
-			todo.setId(++idCounter);
-			todos.add(todo);
-			
-		} else {
-			deleteById(todo.getId());
-			todos.add(todo);
-		}
+		
+		todoRepository.save(todo);
+		
 		return todo;
 	}
 	
-	public Todo deleteById(long id) {
-		Todo todo = findById(id);
-		
-		if(todo==null) return null;
-		
-		if(todos.remove(todo)) {
-			return todo;
-		}
+	public Todo deleteById(Todo todo) {
+	
+		todoRepository.delete(todo);
 		
 		return null;
 	}
 
 	public Todo findById(long id) {
-		for(Todo todo:todos) {
-			if(todo.getId() == id) {
-				return todo;
-			}
-		}
+		
+		todoRepository.findById((int) id);
 		
 		return null;
 	}
--- restfull-web-services/src/main/java/com/example/restfullwebservices/TodoResource.java
+++ restfull-web-services/src/main/java/com/example/restfullwebservices/TodoResource.java
@@ -1,6 +1,5 @@
 package com.example.restfullwebservices;
 
-
 import java.net.URI;
 import java.text.ParseException;
 import java.util.List;
@@ -24,91 +23,78 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-
-
 @RestController
 public class TodoResource {
-	
+
 	@Autowired
 	private TodoHardcodedService todoService;
+
 	
-	@Autowired
-	private TodoRepository todoRepository;
-	
-	
+
 	@GetMapping("/users/{username}/todos")
-	public Iterable<Todo> getAllTodos(@PathVariable String username){
-		return todoRepository.findAll();
+	public Iterable<Todo> getAllTodos(@PathVariable String username) {
+		return todoService.findAll();
 	}
 
-	@GetMapping("/users/{username}/todos/{id}")
-	public Todo getTodo(@PathVariable String username, @PathVariable long id){
+	@GetMapping("/users/todos/{id}")
+	public Todo getTodo(@PathVariable String username, @PathVariable long id) {
 		return todoService.findById(id);
 	}
 
-	//DELETE /users/{username}/todos/{id}
-	@DeleteMapping("/users/{username}/todos/{id}")
-	public ResponseEntity<Void> deleteTodo(
-			@PathVariable String username, @PathVariable long id){
+	// DELETE /users/{username}/todos/{id}
+	@DeleteMapping("/users/todos/{id}")
+	public ResponseEntity<Void> deleteTodo(@RequestBody Todo data) {
+
+		todoService.deleteById(data);
+
 		
-		Todo todo = todoService.deleteById(id);
-		
-		if(todo!=null) {
-			return ResponseEntity.noContent().build();
-		}
-		
+
 		return ResponseEntity.notFound().build();
 	}
-	
 
-	//Edit/Update a Todo
-	//PUT /users/{user_name}/todos/{todo_id}
-	@PutMapping("/users/{username}/todos/{id}")
-	public ResponseEntity<Todo> updateTodo(
-			@PathVariable String username,
-			@PathVariable long id, @RequestBody Todo todo){  //method param is bound to req body
-		
-		Todo todoUpdated = todoService.save(todo);
-		
+/*	// Edit/Update a Todo
+	// PUT /users/{user_name}/todos/{todo_id}
+	@PutMapping("/users/todos/{id}")
+	public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id,
+			@RequestBody Todo todo) { // method param is bound to req body
+ 
+		 todoService.save(todo);
+
 		return new ResponseEntity<Todo>(todo, HttpStatus.OK);
-	}
-	//creating todo
-	@PostMapping("/users/{username}/todos")
-	public ResponseEntity<Void> updateTodo(
-			@PathVariable String username, @RequestBody Todo todo){
-		
+	}*/
+
+	// creating todo
+	@PostMapping("/users/todos/{id}")
+	public ResponseEntity<Void> updateTodo( @RequestBody Todo todo) {
+
 		Todo createdTodo = todoService.save(todo);
-		
-		//Location
-		//Get current resource url
-		///{id}
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(createdTodo.getId()).toUri();
-		
+
+		// Location
+		// Get current resource url
+		/// {id}
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdTodo.getId())
+				.toUri();
+
 		return ResponseEntity.created(uri).build();
 	}
-		
+
 	@RequestMapping(value = "/add", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Todo> addData(@RequestBody Todo data)   {
-		
-		
-		
+	public ResponseEntity<Todo> addData(@RequestBody Todo data) {
+
 		Todo todo = new Todo();
-		
+
 		System.out.println(data.getUsername());
 		todo.setUsername(data.getUsername());
 		todo.setTargetDate(data.getTargetDate());
 		todo.setDescription(data.getDescription());
 		todo.setDone(true);
-		todoRepository.save(todo);
-		
-		
-			
-		
+		todoService.save(todo);
+
 		return null;
 	}
-	
+
 }
