2f330218e32d0fb8356f1de9efe868f7dd410d8e
sarthak-97 <sarthakgupta115@gmail.com>
2021-01-17 22:33:19 +0530

    CRUD changed to JPA

--- restfull-web-services/src/main/java/com/example/restfullwebservices/TodoRepository.java
+++ restfull-web-services/src/main/java/com/example/restfullwebservices/TodoRepository.java
@@ -1,7 +1,8 @@
 package com.example.restfullwebservices;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TodoRepository extends CrudRepository<Todo,Integer>{
+public interface TodoRepository extends JpaRepository<Todo,Integer>{
 
 }
